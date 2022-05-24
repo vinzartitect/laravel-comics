08f47942ccea7eff0f7080ca5f78d8ba74d57472
VincenzoBorriello89 <vincborr89@gmail.com>
2022-05-24 17:49:44 +0200

    esercizio completato

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -4,37 +4,38 @@
 
 @section('detail')
     {{-- @dump($serie) --}}
-    <div class="container-comics-card">
+    <div class="container-comics-card-detail">
         <div class="container-card">
-
             <div class="card-comics">
                 <img src="{{ $serie['thumb'] }}" alt="{{ $serie['title'] }}">
+            </div>
+            <div class="card-info">
                 <h3>"{{ $serie['title'] }}"</h3>
                 <h5>"{{ $serie['price'] }}"</h5>
                 <p>"{{ $serie['description'] }}"</p>
             </div>
-
-            @foreach ($serie['artists'] as $artist)
-                <div>
-                    <h4>ARTIST</h4>
-                    <ul>
-                        <li>{{ $artist }}</li>
-                    </ul>
-                </div>
-            @endforeach
-
         </div>
 
-        @foreach ($serie['writers'] as $writer)
+        <div class="container-info">
+            <div>
+                <h4>ARTIST</h4>
+                @foreach ($serie['artists'] as $artist)
+                    <ul>
+                        <li class="artist">{{ $artist }}</li>
+                    </ul>
+                @endforeach
+            </div>
             <div>
                 <h4>WRITER</h4>
-                <ul>
-                    <li>{{ $writer }}</li>
-                </ul>
+                @foreach ($serie['writers'] as $writer)
+                    <ul>
+                        <li class="writers">{{ $writer }}</li>
+                    </ul>
+                @endforeach
             </div>
-        @endforeach
+        </div>
 
-        <div class="text-container">
+        <div class="text-container-detail">
             <a href="/comics">
                 <h3 class="txt">BACK COMICS</h3>
             </a>
