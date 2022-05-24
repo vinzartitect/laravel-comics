625b9f6390bab9371a41b463aa988fe5f387cd4a
VincenzoBorriello89 <vincborr89@gmail.com>
2022-05-24 12:09:45 +0200

    creazione veloce sezione cards

--- resources/views/pages/comics-home.blade.php
+++ resources/views/pages/comics-home.blade.php
@@ -4,9 +4,59 @@
         <div class="hero">
             <img src="{{url('img/jumbotron.jpg')}}" alt="">
         </div>
-        <h1>
-            home content
-        </h1>
-        <a href="{{'details'}}">Go to details</a>
+        <span class="label">CURRENT SERIES</span>
+        <div class="comics-cont">
+            <div class="comic-card">
+                <a href="{{'details'}}"><img src="{{url('https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX')}}" alt=""></a>
+                <span><a href="{{'details'}}">ACTION COMICS</a></span>
+            </div>
+            <div class="comic-card">
+                <a href="{{'details'}}"><img src="{{url('https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX')}}" alt=""></a>
+                <span><a href="{{'details'}}">ACTION COMICS</a></span>
+            </div>
+            <div class="comic-card">
+                <a href="{{'details'}}"><img src="{{url('https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX')}}" alt=""></a>
+                <span><a href="{{'details'}}">ACTION COMICS</a></span>
+            </div>
+            <div class="comic-card">
+                <a href="{{'details'}}"><img src="{{url('https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX')}}" alt=""></a>
+                <span><a href="{{'details'}}">ACTION COMICS</a></span>
+            </div>
+            <div class="comic-card">
+                <a href="{{'details'}}"><img src="{{url('https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX')}}" alt=""></a>
+                <span><a href="{{'details'}}">ACTION COMICS</a></span>
+            </div>
+            <div class="comic-card">
+                <a href="{{'details'}}"><img src="{{url('https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX')}}" alt=""></a>
+                <span><a href="{{'details'}}">ACTION COMICS</a></span>
+            </div>
+            <div class="comic-card">
+                <a href="{{'details'}}"><img src="{{url('https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX')}}" alt=""></a>
+                <span><a href="{{'details'}}">ACTION COMICS</a></span>
+            </div>
+            <div class="comic-card">
+                <a href="{{'details'}}"><img src="{{url('https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX')}}" alt=""></a>
+                <span><a href="{{'details'}}">ACTION COMICS</a></span>
+            </div>
+            <div class="comic-card">
+                <a href="{{'details'}}"><img src="{{url('https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX')}}" alt=""></a>
+                <span><a href="{{'details'}}">ACTION COMICS</a></span>
+            </div>
+            <div class="comic-card">
+                <a href="{{'details'}}"><img src="{{url('https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX')}}" alt=""></a>
+                <span><a href="{{'details'}}">ACTION COMICS</a></span>
+            </div>
+            <div class="comic-card">
+                <a href="{{'details'}}"><img src="{{url('https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX')}}" alt=""></a>
+                <span><a href="{{'details'}}">ACTION COMICS</a></span>
+            </div>
+            <div class="comic-card">
+                <a href="{{'details'}}"><img src="{{url('https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX')}}" alt=""></a>
+                <span><a href="{{'details'}}">ACTION COMICS</a></span>
+            </div>            
+        </div>
+        <div class="btn">
+            <a href="" class="btn-link">LOAD MORE</a>
+        </div>
     </main>
 @endsection
